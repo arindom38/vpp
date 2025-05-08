16b1e21c98c6d91787667e8ca29f09a29596b278
ujjal38 <kundu.vivasoft@bkash.com>
2025-05-08 16:42:39 +0600

    VPP-0: updated the column type of battery (id, created_at, modified_at)

--- src/main/resources/db/migration/V1__create_table_battery.sql
+++ src/main/resources/db/migration/V1__create_table_battery.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS battery (
-    id  BIGINT PRIMARY KEY,
+    id  bigserial PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     postcode INT NOT NULL,
     watt_capacity BIGINT NOT NULL,
-    created_at  TIMESTAMP NOT NULL,
-    modified_at TIMESTAMP NOT NULL
+    created_at  timestamp with time zone NOT NULL,
+    modified_at timestamp with time zone NOT NULL
 );
