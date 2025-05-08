135d48bae5eff1d68ffce3d5d44a587acd6cd10c
ujjal38 <kundu.vivasoft@bkash.com>
2025-05-08 10:14:37 +0600

    VPP-0: Project initialization

---
+++ src/test/java/com/challenge/vpp/VppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.vpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
